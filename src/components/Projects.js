3b40277f983f90f2deed2eb6d2203b7035c592c5
juanib12 <bjuan.ignacio11@gmail.com>
2023-02-27 10:12:10 -0300

    add project firechat

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,6 +11,7 @@ import Node from "../images/node.png";
 import Mongo from "../images/mongodbicon.png";
 import findflix from "../images/findflix.png";
 import tmdb from "../images/tmdb.png";
+import firechat from '../images/firechat.png'
 
 const Projects = () => {
   return (
@@ -110,6 +111,30 @@ const Projects = () => {
               <div className="projects_data-desc">
                 App divertida para encontrar información, servicios de streaming
                 y muchas cosas más de tus películas favoritas.
+              </div>
+              <button>Ver más</button>
+            </div>
+          </div>
+        </a>
+
+        <a target="_blank" href="http://juani-chat-app.vercel.app/">
+          <div className="projects_data">
+            <div className="projects_data-images">
+              <img src={firechat} />
+            </div>
+            <div className="projects_data-info">
+              <div className="projects_data-title">
+                <h2>FireChat - Messages App</h2>
+              </div>
+              <div className="projects_data-responsive">
+                <p>Responsive</p>
+              </div>
+              <div className="projects_data-leng">
+                <img src={ReactChiquito} width="50px" />
+                <img src={Firebase} width="50px" />
+              </div>
+              <div className="projects_data-desc">
+                Una web app para recibir y enviar mensajes a cualquier usuario que este registrado en la app.
               </div>
               <button>Ver más</button>
             </div>
