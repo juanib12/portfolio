6a91061d3a76c7eb124596521cd4af6afcb16ab3
juaniB12 <bjuan.ignacio11@gmail.com>
2024-04-23 10:05:35 -0300

    change img

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
-import Profile from '../images/profile.png'
+import Profile from '../images/juani2.jpg'
 
 const Home = () => {
   return (
     <section className="bd-grid">
       <div className="home">
-        <img src={Profile} alt="profile" className="avatar"/>
+        <div style={{width: '15em'}}>
+          <img src={Profile} alt="profile" className="avatar"/>
+        </div>
         <div className="home_title">
           <h2>Hola! Soy</h2>
           <h1>Juan Bianco</h1>
