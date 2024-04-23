b1054a2186066e19df6cd8a3454115496368d022
juaniB12 <bjuan.ignacio11@gmail.com>
2024-04-23 20:23:30 -0300

    add background

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,33 +9,36 @@ const Contact = () => {
   };
 
   return (
-      <section className="bd-grid">
-        <div className="contact">
-          <div className="contact-data_list">
-            <a href="https://github.com/juanib12" target="__blank">
-              <div className="contact-data-images">
-                <img src={Github} loading="lazy" alt="logo airbnb" />
+      <>
+        <div className="background"></div>
+        <section className="bd-grid">
+          <div className="contact">
+            <div className="contact-data_list">
+              <a href="https://github.com/juanib12" target="__blank">
+                <div className="contact-data-images">
+                  <img src={Github} loading="lazy" alt="logo airbnb" />
+                </div>
+              </a>
+              <a href="https://www.instagram.com/bianco_juani/" target="__blank">
+                <div className="contact-data-images">
+                  <img src={IG} loading="lazy" alt="logo microsoft" />
+                </div>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/juanignaciobianco/"
+                target="__blank"
+              >
+                <div className="contact-data-images">
+                  <img src={Linkedin} loading="lazy" alt="logo ge" />
+                </div>
+              </a>
+              <div onClick={gmailAlert} className="contact-data-images">
+                <img src={Gmail} loading="lazy" alt="logo ge" />
               </div>
-            </a>
-            <a href="https://www.instagram.com/bianco_juani/" target="__blank">
-              <div className="contact-data-images">
-                <img src={IG} loading="lazy" alt="logo microsoft" />
-              </div>
-            </a>
-            <a
-              href="https://www.linkedin.com/in/juanignaciobianco/"
-              target="__blank"
-            >
-              <div className="contact-data-images">
-                <img src={Linkedin} loading="lazy" alt="logo ge" />
-              </div>
-            </a>
-            <div onClick={gmailAlert} className="contact-data-images">
-              <img src={Gmail} loading="lazy" alt="logo ge" />
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </>
   );
 };
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -27,149 +27,152 @@ const Projects = () => {
 
 
   return (
-    <section className="projects bd-grid">
-      <div className="main-title">
-        <h1>Proyectos ✅</h1>
-      </div>
-      <div className="projects_list">
-      <a
-          target="_blank"
-          href="https://findflix.vercel.app/"
-          className="links"
-        >
-          <div className="projects_data">
-            <div className="projects_data-images">
-              <img src={findflix} />
-            </div>
-            <div className="projects_data-info">
-              <div className="projects_data-title">
-                <h2>FindFlix</h2>
-              </div>
-              <div className="projects_data-responsive">
-                <p>Responsive</p>
-              </div>
-              <div className="projects_data-leng">
-                <img src={ReactChiquito} width="50px" />
-                <img src={tmdb} width="50px" />
-              </div>
-              <div className="projects_data-desc">
-                App divertida para encontrar información, servicios de streaming
-                y muchas cosas más de tus películas favoritas.
-              </div>
-            </div>
-          </div>
-        </a>
-
+    <>
+      <div className="background"></div>
+      <section className="projects bd-grid">
+        <div className="main-title">
+          <h1>Proyectos ✅</h1>
+        </div>
+        <div className="projects_list">
         <a
-          target="_blank"
-          href="https://toodnotes.vercel.app/"
-          className="links"
-        >
-          <div className="projects_data">
-            <div className="projects_data-images">
-              <img src={Tood} />
+            target="_blank"
+            href="https://findflix.vercel.app/"
+            className="links"
+          >
+            <div className="projects_data">
+              <div className="projects_data-images">
+                <img src={findflix} />
+              </div>
+              <div className="projects_data-info">
+                <div className="projects_data-title">
+                  <h2>FindFlix</h2>
+                </div>
+                <div className="projects_data-responsive">
+                  <p>Responsive</p>
+                </div>
+                <div className="projects_data-leng">
+                  <img src={ReactChiquito} width="50px" />
+                  <img src={tmdb} width="50px" />
+                </div>
+                <div className="projects_data-desc">
+                  App divertida para encontrar información, servicios de streaming
+                  y muchas cosas más de tus películas favoritas.
+                </div>
+              </div>
             </div>
-            <div className="projects_data-info">
-              <div className="projects_data-title">
-                <h2>Tood - Notes</h2>
-              </div>
-              <div className="projects_data-responsive">
-                <p>Responsive</p>
-              </div>
-              <div className="projects_data-leng">
-                <img src={ReactChiquito} width="50px" />
-                <img src={Mui} width="50px" />
-                <img src={Firebase} width="50px" />
-              </div>
-              <div className="projects_data-desc">
-                App donde podrás añadir, editar o eliminar tus notas y marcarlas
-                como completado.
-              </div>
-            </div>
-          </div>
-        </a>
+          </a>
 
-        <a
-          href="https://animadle.vercel.app/"
-          target="_blank"
-          className="links"
-        >
-          <div className="projects_data">
-            <div className="projects_data-images">
-              <img src={Animadle} />
+          <a
+            target="_blank"
+            href="https://toodnotes.vercel.app/"
+            className="links"
+          >
+            <div className="projects_data">
+              <div className="projects_data-images">
+                <img src={Tood} />
+              </div>
+              <div className="projects_data-info">
+                <div className="projects_data-title">
+                  <h2>Tood - Notes</h2>
+                </div>
+                <div className="projects_data-responsive">
+                  <p>Responsive</p>
+                </div>
+                <div className="projects_data-leng">
+                  <img src={ReactChiquito} width="50px" />
+                  <img src={Mui} width="50px" />
+                  <img src={Firebase} width="50px" />
+                </div>
+                <div className="projects_data-desc">
+                  App donde podrás añadir, editar o eliminar tus notas y marcarlas
+                  como completado.
+                </div>
+              </div>
             </div>
-            <div className="projects_data-info">
-              <div className="projects_data-title">
-                <h2>Animadle</h2>
-              </div>
-              <div className="projects_data-leng">
-                <img src={ReactChiquito} width="50px" />
-                <img src={Mui} width="50px" />
-              </div>
-              <div className="projects_data-desc">
-                Juego al estilo wordle, donde deberás adivinar el animal de 5
-                letras.
-              </div>
-            </div>
-          </div>
-        </a>
+          </a>
 
-        <a
-          target="_blank"
-          href="https://techfix-demo.vercel.app/"
-          className="links"
-        >
-          <div className="projects_data">
-            <div className="projects_data-images">
-              <img src={Techfix} />
+          <a
+            href="https://animadle.vercel.app/"
+            target="_blank"
+            className="links"
+          >
+            <div className="projects_data">
+              <div className="projects_data-images">
+                <img src={Animadle} />
+              </div>
+              <div className="projects_data-info">
+                <div className="projects_data-title">
+                  <h2>Animadle</h2>
+                </div>
+                <div className="projects_data-leng">
+                  <img src={ReactChiquito} width="50px" />
+                  <img src={Mui} width="50px" />
+                </div>
+                <div className="projects_data-desc">
+                  Juego al estilo wordle, donde deberás adivinar el animal de 5
+                  letras.
+                </div>
+              </div>
             </div>
-            <div className="projects_data-info">
-              <div className="projects_data-title">
-                <h2>Techfix</h2>
-              </div>
-              <div className="projects_data-leng">
-                <img src={ReactChiquito} width="50px" />
-                <img src={Mui} width="50px" />
-                <img src={Mongo} width="50px" />
-                <img src={Node} width="50px" />
-              </div>
-              <div className="projects_data-desc">
-                App donde podrás añadir, editar o eliminar tus servicios para tu
-                negocio de servicio técnico.
-              </div>
-            </div>
-          </div>
-        </a>
+          </a>
 
-        <a
-          target="_blank"
-          href="https://juani-chat-app.vercel.app/"
-          className="links"
-        >
-          <div className="projects_data">
-            <div className="projects_data-images">
-              <img src={firechat} />
+          <a
+            target="_blank"
+            href="https://techfix-demo.vercel.app/"
+            className="links"
+          >
+            <div className="projects_data">
+              <div className="projects_data-images">
+                <img src={Techfix} />
+              </div>
+              <div className="projects_data-info">
+                <div className="projects_data-title">
+                  <h2>Techfix</h2>
+                </div>
+                <div className="projects_data-leng">
+                  <img src={ReactChiquito} width="50px" />
+                  <img src={Mui} width="50px" />
+                  <img src={Mongo} width="50px" />
+                  <img src={Node} width="50px" />
+                </div>
+                <div className="projects_data-desc">
+                  App donde podrás añadir, editar o eliminar tus servicios para tu
+                  negocio de servicio técnico.
+                </div>
+              </div>
             </div>
-            <div className="projects_data-info">
-              <div className="projects_data-title">
-                <h2>FireChat</h2>
+          </a>
+
+          <a
+            target="_blank"
+            href="https://juani-chat-app.vercel.app/"
+            className="links"
+          >
+            <div className="projects_data">
+              <div className="projects_data-images">
+                <img src={firechat} />
               </div>
-              <div className="projects_data-responsive">
-                <p>Responsive</p>
-              </div>
-              <div className="projects_data-leng">
-                <img src={ReactChiquito} width="50px" />
-                <img src={Firebase} width="50px" />
-              </div>
-              <div className="projects_data-desc">
-                Una app para enviar y recibir mensajes en tiempo real a los
-                usuarios que esten registrados.
+              <div className="projects_data-info">
+                <div className="projects_data-title">
+                  <h2>FireChat</h2>
+                </div>
+                <div className="projects_data-responsive">
+                  <p>Responsive</p>
+                </div>
+                <div className="projects_data-leng">
+                  <img src={ReactChiquito} width="50px" />
+                  <img src={Firebase} width="50px" />
+                </div>
+                <div className="projects_data-desc">
+                  Una app para enviar y recibir mensajes en tiempo real a los
+                  usuarios que esten registrados.
+                </div>
               </div>
             </div>
-          </div>
-        </a>
-      </div>
-    </section>
+          </a>
+        </div>
+      </section>
+    </>
   );
 };
 
