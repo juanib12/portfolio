7e1af325ec83880a17c873d98af9f7534cefc699
juaniB12 <bjuan.ignacio11@gmail.com>
2024-04-26 11:48:30 -0300

    changes styles

--- src/components/CV.js
+++ src/components/CV.js
@@ -3,6 +3,7 @@ import _cv from '../images/cv.png'
 const CV = () => {
     return (
         <>
+            <div className="background"></div>
             <img src={_cv} alt='cv' className='cv'/>
             <div className="home_actions cv-actions">
                 <a href='https://drive.google.com/file/d/1YU6H1xM0zA04VkZUBfN7-v2qG1GDJVxA/view?usp=sharing' target='__blank' className="home_actions-cv">Descargar</a>
