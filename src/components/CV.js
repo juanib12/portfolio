ec366eb45a3601388f27bfac28e8266a22c379ec
juaniB12 <bjuan.ignacio11@gmail.com>
2024-04-26 11:45:49 -0300

    add cv

--- src/components/CV.js
+++ src/components/CV.js
@@ -1,11 +1,11 @@
-import _cv from '../images/cv.jpg' 
+import _cv from '../images/cv.png' 
 
 const CV = () => {
     return (
         <>
             <img src={_cv} alt='cv' className='cv'/>
             <div className="home_actions cv-actions">
-                <a href='https://drive.google.com/file/d/1uXhbRizZkEx0LcWYqAAn8hvoHFf3swN_/view?usp=sharing' target='__blank' className="home_actions-cv">Descargar</a>
+                <a href='https://drive.google.com/file/d/1YU6H1xM0zA04VkZUBfN7-v2qG1GDJVxA/view?usp=sharing' target='__blank' className="home_actions-cv">Descargar</a>
             </div>
         </>
     )
