af46bfcb865cb9940cf4081639d13c88203e9b5a
juaniB12 <bjuan.ignacio11@gmail.com>
2024-07-06 11:52:06 -0300

    add new cv

--- src/components/CV.js
+++ src/components/CV.js
@@ -6,7 +6,7 @@ const CV = () => {
             <div className="background"></div>
             <img src={_cv} alt='cv' className='cv'/>
             <div className="home_actions cv-actions">
-                <a href='https://drive.google.com/file/d/1ln4Ff-FYK73_Bnj-O5v5Vxsd8sSkfnNz/view?usp=sharing' target='__blank' className="home_actions-cv">Descargar</a>
+                <a href='https://drive.google.com/file/d/1ovXEn9qXjApPWKV1GdHL5SsePZFpGqA1/view?usp=sharing' target='__blank' className="home_actions-cv">Descargar</a>
             </div>
         </>
     )
