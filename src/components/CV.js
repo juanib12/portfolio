4eec1fc5e24ba058b781e9bc7bf60941fe9ec519
Juan Bianco <83496310+juanib12@users.noreply.github.com>
2025-02-18 11:14:43 -0300

    Update CV.js

--- src/components/CV.js
+++ src/components/CV.js
@@ -6,7 +6,7 @@ const CV = () => {
             <div className="background"></div>
             <img src={_cv} alt='cv' className='cv'/>
             <div className="home_actions cv-actions">
-                <a href='https://drive.google.com/file/d/1ovXEn9qXjApPWKV1GdHL5SsePZFpGqA1/view?usp=sharing' target='__blank' className="home_actions-cv">Descargar</a>
+                <a href='https://drive.google.com/file/d/1EHXymBvnRgmS7x5T0PPbsu2sku-yRKln/view?usp=sharing' target='__blank' className="home_actions-cv">Descargar</a>
             </div>
         </>
     )
