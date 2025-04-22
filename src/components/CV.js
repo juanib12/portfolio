2f3aa29d90bc40112eab88b25480e3c4c9b4ca3d
juanib12 <bjuan.ignacio11@gmail.com>
2025-04-22 14:48:48 -0300

    cambios cv

--- src/components/CV.js
+++ src/components/CV.js
@@ -1,4 +1,4 @@
-import _cv from '../images/cv.jpg' 
+import _cv from '../images/cv.png' 
 
 const CV = () => {
     return (
@@ -6,7 +6,7 @@ const CV = () => {
             <div className="background"></div>
             <img src={_cv} alt='cv' className='cv'/>
             <div className="home_actions cv-actions">
-                <a href='https://drive.google.com/file/d/1EHXymBvnRgmS7x5T0PPbsu2sku-yRKln/view?usp=sharing' target='__blank' className="home_actions-cv">Descargar</a>
+                <a href='https://drive.google.com/file/d/18CQbopmULlXxoKrg-gS6MlK_gVEhaO9z/view?usp=sharing' target='__blank' className="home_actions-cv">Descargar</a>
             </div>
         </>
     )
