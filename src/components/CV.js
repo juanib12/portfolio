8ecb1f7b4edb53ac4e1f40a4dd5e7d63cbef8fb1
juanib12 <bjuan.ignacio11@gmail.com>
2026-06-29 10:46:25 -0300

    change CV

--- src/components/CV.js
+++ src/components/CV.js
@@ -6,7 +6,7 @@ const CV = () => {
             <div className="background"></div>
             <Image src="/images/cv.png" alt='cv' className='cv' width={640} height={900}/>
             <div className="home_actions cv-actions">
-                <a href='https://drive.google.com/file/d/18CQbopmULlXxoKrg-gS6MlK_gVEhaO9z/view?usp=sharing' target='__blank' className="home_actions-cv">Descargar</a>
+                <a href='https://drive.google.com/file/d/1U_Iv4wLKGwYF0OUwM3SZPC-sMVKH1FhL/view?usp=sharing' target='__blank' className="home_actions-cv">Descargar</a>
             </div>
         </>
     )
